80a8efe60bd9eb842275f9f6ffb7a3a9ac2a19c5
Benji2705 <49046441+Benji2705@users.noreply.github.com>
2023-01-17 03:51:28 +0000

    Add files via upload
    Signed-off-by: Benji2705 <49046441+Benji2705@users.noreply.github.com>

---
+++ app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReadXlsxFileComponent } from './components/read-xlsx-file/read-xlsx-file.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { VideosComponent } from './components/dashboard/videos/videos.component';
+import { ImagesComponent } from './components/dashboard/images/images.component';
+import { FichiersComponent } from './components/dashboard/fichiers/fichiers.component';
+
+const routes: Routes = [
+ /* {path: "", component: UploadFileComponent},
+  {path: "excel", component: ReadXlsxFileComponent},
+  {path: "uploadFile", component: UploadFileComponent}*/
+  {path: 'dashboard', children : [
+    {path:'', redirectTo:'videos', pathMatch:'full'},
+    {path:'videos', component: VideosComponent},
+    {path:'images', component: ImagesComponent},
+    {path:'fichiers', component: FichiersComponent}
+    
+  ] },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFunctions,getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReadXlsxFileComponent } from './components/read-xlsx-file/read-xlsx-file.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { MaterialModule } from './material/material/material.module';
+import { VideosComponent } from './components/dashboard/videos/videos.component';
+import { ImagesComponent } from './components/dashboard/images/images.component';
+import { FichiersComponent } from './components/dashboard/fichiers/fichiers.component';
+import { SidebarComponent } from './components/dashboard/sidebar/sidebar.component';
+import { AddFilesComponent } from './components/dashboard/fichiers/add-files/add-files.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReadXlsxFileComponent,
+    UploadFileComponent,
+    VideosComponent,
+    ImagesComponent,
+    FichiersComponent,
+    SidebarComponent,
+    AddFilesComponent,
+  ],
+  imports: [
+    MaterialModule,
+    AngularFireModule,
+    AngularFirestoreModule,   
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => {
+      const functions = getFunctions()
+      if (!environment.production) {
+        connectFunctionsEmulator(functions, 'localhost', 5001)
+      }
+      return functions
+    }),
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule
+
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ 
+    AddFilesComponent,
+  ]
+})
+export class AppModule { }
